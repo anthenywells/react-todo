56d2f6997fa4fa7d8e64044424d94c0e07c23223
Antheny Wells <anthenywells@Go-away.local>
2019-03-27 01:00:39 -0400

    build / gh pages optimization

---
+++ docs/precache-manifest.34f5ad2ce712b84a458a43897c5ed3e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f1e1c7ed5de42b608233",
+    "url": "/static/js/main.c766550b.chunk.js"
+  },
+  {
+    "revision": "f36054a2f6c5bf348fa1",
+    "url": "/static/js/2.4214afab.chunk.js"
+  },
+  {
+    "revision": "c45e1b7794a0074809fb5d30f8c97020",
+    "url": "/index.html"
+  }
+];
